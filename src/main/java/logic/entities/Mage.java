8af17c397f13ff305a38c5424e761486ba4d904a
dima493 <80508838+dima493@users.noreply.github.com>
2025-10-25 14:46:00 +0300

    Update Mage.java

--- src/main/java/logic/entities/Mage.java
+++ src/main/java/logic/entities/Mage.java
@@ -1,18 +1,32 @@
 package logic.entities;
 
-import logic.actions.Calculation;
-
 public class Mage extends Unit {
+
+    // Старий конструктор
     public Mage(String name, int y, int x, Map map) {
         super(name, 100, 5, 10, 18, 2, 15, 5, 2, y, x, map);
     }
 
+    /**
+     * Новий конструктор для "покупки" (без координат).
+     */
+    public Mage(String name, Map map) {
+        this(name, -1, -1, map);
+    }
+
     @Override
     public boolean attack(Unit target) {
-        if (getActions() > 0 && target != null && Calculation.difference(getX(), target.getX()) <= getDistanceOfAtk() && Calculation.difference(getY(), target.getX()) <= getDistanceOfAtk()) {
-            target.setHp(target.getHp() - getDamage() + getIntelligence());
-            setActions(getActions() - 1);
-            return true;
+        if (getActions() > 0 && target != null) {
+            int distance = Math.max(
+                Math.abs(getX() - target.getX()),
+                Math.abs(getY() - target.getY()) // Виправлено з target.getX()
+            );
+
+            if (distance <= getDistanceOfAtk()) {
+                target.setHp(target.getHp() - (getDamage() + getIntelligence()));
+                setActions(getActions() - 1);
+                return true;
+            }
         }
         return false;
     }
